cab992631fcd7cc2637f114103667bfef71516a7
Pan, Wen-Ming <smalljelly2018@gmail.com>
2023-11-15 13:04:50 +0800

    Update start.sh

--- sd-webui/start.sh
+++ sd-webui/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 user-watch.py &
-python3 webui.py --listen --xformers --medvram --enable-insecure-extension-access --api
+python3 webui.py --listen --xformers --enable-insecure-extension-access --disable-nan-check --api

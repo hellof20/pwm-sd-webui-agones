97da3f59515d1576840b6aec8816f6be8a2907f6
Wenming Pan <pwm@google.com>
2023-11-25 07:26:43 +0000

    20231125 update

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ export FILESTORE_NAME=sdfilestore
 export FILESTORE_ZONE=us-central1-b
 export FILESHARE_NAME=sd
 export BUILD_REGIST=sd-repo
-export SD_WEBUI_IMAGE=${REGION}-docker.pkg.dev/${PROJECT_ID}/${BUILD_REGIST}/sd-webui:0.1
+export SD_WEBUI_IMAGE=${REGION}-docker.pkg.dev/${PROJECT_ID}/${BUILD_REGIST}/sd-webui:5
 export NGINX_IMAGE=${REGION}-docker.pkg.dev/${PROJECT_ID}/${BUILD_REGIST}/sd-nginx:0.1
 export AGONES_SIDECAR_IMAGE=${REGION}-docker.pkg.dev/${PROJECT_ID}/${BUILD_REGIST}/sd-agones-sidecar:0.1
 export DOMAIN_NAME=sd.joey618.top
@@ -28,16 +28,38 @@ gcloud container --project ${PROJECT_ID} clusters create ${GKE_CLUSTER_NAME} --r
     --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver,GcpFilestoreCsiDriver \
     --autoscaling-profile optimize-utilization \
     --enable-image-streaming
-gcloud container --project ${PROJECT_ID} node-pools create "gpu-pool" --cluster ${GKE_CLUSTER_NAME} --region ${REGION} --node-locations ${FILESTORE_ZONE} --machine-type "g2-standard-4" --accelerator "type=nvidia-l4,count=1" --image-type "COS_CONTAINERD" --disk-type "pd-balanced" --disk-size "200" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/cloud-platform" --enable-autoscaling --total-min-nodes "1" --total-max-nodes "6" --location-policy "ANY" --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --max-pods-per-node "110" --num-nodes "1"
+
+gcloud container --project ${PROJECT_ID} node-pools create "gpu-pool" \
+--cluster ${GKE_CLUSTER_NAME} \
+--region ${REGION} \
+--node-locations ${FILESTORE_ZONE} \
+--machine-type "g2-standard-4" --accelerator "type=nvidia-l4,count=1" \
+--image-type "COS_CONTAINERD" --disk-type "pd-balanced" --disk-size "200" \
+--metadata disable-legacy-endpoints=true \
+--scopes "https://www.googleapis.com/auth/cloud-platform" \
+--enable-autoscaling --total-min-nodes "1" --total-max-nodes "6" \
+--location-policy "ANY" --enable-autoupgrade \
+--enable-autorepair \
+--max-surge-upgrade 1 --max-unavailable-upgrade 0 \
+--max-pods-per-node "110" --num-nodes "1"
 
 
 ## Redis
-gcloud redis instances create --project=${PROJECT_ID}  sd-agones-cache --tier=standard --size=1 --region=${REGION} --redis-version=redis_6_x --network=projects/${PROJECT_ID}/global/networks/${VPC_NETWORK} --connect-mode=DIRECT_PEERING
-export REDIS_IP=$(gcloud redis instances describe sd-agones-cache --region ${REGION} --format=json 2>/dev/null | jq -r .host)
+gcloud redis instances create sd-agones-cache \
+--project=${PROJECT_ID} \
+--tier=standard --size=1 --region=${REGION} \
+--redis-version=redis_6_x \
+--network=projects/${PROJECT_ID}/global/networks/${VPC_NETWORK} \
+--connect-mode=DIRECT_PEERING
+export REDIS_IP=$(gcloud redis instances describe sd-agones-cache --project=${PROJECT_ID} --region ${REGION} --format=json 2>/dev/null | jq -r .host)
 
 
 ## Filestore
-gcloud filestore instances create ${FILESTORE_NAME} --zone=${FILESTORE_ZONE} --tier=BASIC_HDD --file-share=name=${FILESHARE_NAME},capacity=1TB --network=name=${VPC_NETWORK}
+gcloud filestore instances create ${FILESTORE_NAME} \
+--project=${PROJECT_ID} --zone=${FILESTORE_ZONE} \
+--tier=BASIC_HDD \
+--file-share=name=${FILESHARE_NAME},capacity=1TB \
+--network=name=${VPC_NETWORK}
 export FILESTORE_IP=$(gcloud filestore instances describe ${FILESTORE_NAME} --project=${PROJECT_ID} --zone=${FILESTORE_ZONE} --format json |jq -r .networks[].ipAddresses[])
 
 
@@ -45,9 +67,8 @@ export FILESTORE_IP=$(gcloud filestore instances describe ${FILESTORE_NAME} --pr
 helm repo add agones https://agones.dev/chart/stable
 helm repo update
 kubectl create namespace agones-system
-cd agones
-helm install sd-agones-release --namespace agones-system -f values.yaml agones/agones --version 1.30.0
-gcloud compute --project=speedy-victory-336109 firewall-rules create agones-sd-firewall \
+helm install sd-agones-release --namespace agones-system -f agones/values.yaml agones/agones --version 1.30.0
+gcloud compute --project=${PROJECT_ID} firewall-rules create agones-sd-firewall \
 --network=${VPC_NETWORK} \
 --action=ALLOW \
 --rules=tcp:7000-8000,tcp:8080,tcp:8081,udp:7000-8000 \
@@ -61,8 +82,8 @@ gcloud auth configure-docker ${REGION}-docker.pkg.dev
 
 
 ## build sd webui image
-cd Stable-Diffusion-on-GCP/Stable-Diffusion-UI-Agones/sd-webui
-docker build . -t $SD_WEBUI_IMAGE
+cd sd-webui
+DOCKER_BUILDKIT=1 docker build . -t $SD_WEBUI_IMAGE
 docker push $SD_WEBUI_IMAGE
 ## Build nginx proxy image
 cd ../nginx
@@ -78,7 +99,6 @@ docker push $AGONES_SIDECAR_IMAGE
 ## Deploy stable-diffusion
 gcloud container clusters get-credentials ${GKE_CLUSTER_NAME} --region ${REGION}
 kubectl apply -f https://raw.githubusercontent.com/GoogleCloudPlatform/container-engine-accelerators/master/nvidia-driver-installer/cos/daemonset-preloaded-latest.yaml
-cd ..
 envsubst < agones/nfs_pv_pvc.yaml  | kubectl apply -f -
 envsubst < agones/fleet.yaml  | kubectl apply -f -
 kubectl apply -f agones/fleet_autoscale.yaml
@@ -111,4 +131,3 @@ envsubst < ingress-iap/managed-cert.yaml | kubectl apply -f -
 kubectl apply -f ingress-iap/backendconfig.yaml
 kubectl apply -f ingress-iap/service.yaml
 kubectl apply -f ingress-iap/ingress.yaml
-
